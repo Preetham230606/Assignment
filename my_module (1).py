4799cdf4032189f96dca0b37ccd85c580e42b4f6
Preetham230606 <2311cs020007@mallareddyuniversity.ac.in>
2025-01-09 13:22:40 +0530

    Add files via upload

---
+++ my_module (1).py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+greet = lambda name : print(f"Hello {name}!")
+
+
+# In[9]:
+
+
+greet("Good Morning")
+
+
+# In[3]:
+
+
+product = lambda a,b,c : a*b*c
+
+
+# In[4]:
+
+
+product(20,30,40)
+
+
+# In[5]:
+
+
+even = lambda L : [x for x in L if x%2 ==0]
+
+
+# In[6]:
+
+
+my_list = [100,3,9,38,43,56,20]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
